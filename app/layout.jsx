126809e1543434e985085f591b5c1d76d82925c4
Dilbacker <mmdilbacker@gmail.com>
2023-06-08 17:52:04 +0530

    added pages

---
+++ app/layout.jsx
@@ -0,0 +1,17 @@
+import '@styles/globals.css';
+
+
+export const metadata = { 
+    title: "Promtopia",
+    description: 'Discover & Share AI prompts'
+
+}
+
+
+const layout = () => {
+  return (
+    <div>layout</div>
+  )
+}
+
+export default layout
